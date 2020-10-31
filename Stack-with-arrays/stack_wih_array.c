cbc0fa4df592981c51c3990203d7208b4d3e7f86
Peng Boris Akebuon <peng.akebuon2468@gmail.com>
2020-10-31 17:39:03 +0100

    craeate stack_wih_array.c
    craeate stack_wih_array.c and add  define n and array named stacked

---
+++ Stack-with-arrays/stack_wih_array.c
@@ -0,0 +1,12 @@
+/*C program using arrays to implement the stack data structure*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;
+
+
+int main(){
+    printf("\n Enter the size of the Stack");
+    scanf("%d", &n);
+    int stack[n];
+}
